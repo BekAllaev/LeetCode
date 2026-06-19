b322e069eb277714cd2b456bd4b9d8993c350592
Bekzod Allaev <Allaev_@outlook.com>
2026-06-19 13:10:28 +0200

    Add C# project with PlusOne solution and setup
    Added LeetCode.CSharp project targeting .NET 10.0 with nullable and implicit usings enabled. Implemented PlusOne problem solution in PlusOne.cs. Added Program.cs with "Hello, World!" output. Updated solution file to include new project and platform configurations.

---
+++ LeetCode.CSharp/Problems/PlusOne.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.CSharp.Problems;
+
+public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        bool extra = true;
+
+        for (int i = digits.Length - 1; extra && i >= 0; i--)
+        {
+            if (digits[i] == 9)
+            {
+                extra = true;
+                digits[i] = 0;
+            }
+            else
+            {
+                extra = false;
+                digits[i] = digits[i] + 1;
+            }
+        }
+
+        if (!extra)
+            return digits;
+
+        int[] originalDigits = digits;
+        digits = new int[digits.Length + 1];
+
+        digits[0] = 1;
+
+        for (int i = 0; i < originalDigits.Length; i++)
+            digits[i + 1] = originalDigits[i];
+
+        return digits;
+    }
+}

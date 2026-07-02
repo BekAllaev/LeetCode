7b7d6f4e41636793ea2c71985d012c1523234bca
Bekzod Allaev <allaev_@outlook.com>
2026-07-02 17:34:42 +0500

    Add ValidPalindrome class to check for palindromes

---
+++ LeetCode.CSharp/Problems/ValidPalindrome.cs
@@ -0,0 +1,24 @@
+public class Solution 
+{
+    public bool IsPalindrome(string s) 
+    {
+        int left = 0, right = s.Length - 1;
+
+        while (right > left)
+        {
+            while (right > left && !char.IsLetter(s[left]) && !char.IsDigit(s[left]))
+                left++;
+            
+            while (right > left && !char.IsLetter(s[right]) && !char.IsDigit(s[right]))
+                right--;
+
+            if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                return false;
+
+            right--;
+            left++;
+        }    
+
+        return true;
+    }
+}

8784c9ffa9d739e4227ba82810f90376d905f56a
Bekzod Allaev <Allaev_@outlook.com>
2026-07-09 14:50:09 +0200

    Count complete binary tree nodes implementation
    Added CountCompleteTreeNodes.cs with TreeNode and Solution classes. Implemented efficient O((log n)^2) and O(n) algorithms for counting nodes in a complete binary tree. Updated Program.cs to construct a sample tree and output the node count with expected result.

---
+++ LeetCode.CSharp/Problems/CountCompleteTreeNodes.cs
@@ -0,0 +1,87 @@
+﻿namespace CountCompleteTreeNodes;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    private bool IsIdeal(TreeNode root, out int height)
+    {
+        TreeNode? left = root.left, right = root.right;
+        height = 1;
+
+        while (left is not null && right is not null)
+        {
+            left = left.left;
+            right = right.right;
+            height++;
+        }
+
+        return left is null && right is null;
+    }
+
+    // Accepted solution that runs for 0ms and has O((log(n))^2) time complexity
+    // O((log(n))^2) is because we check at least log(n) nodes at each level and we have log(n) levels
+    // so log(n) * log(n) = (log(n))^2
+    public int CountNodes(TreeNode root)
+    {
+        int result = 0;
+        var stack = new Stack<TreeNode?>();
+        stack.Push(root);
+
+        while (stack.Count > 0)
+        {
+            var node = stack.Pop();
+
+            if (node is null)
+                continue;
+
+            var isIdeal = IsIdeal(node, out int height);
+
+            if (isIdeal)
+                result += (1 << height) - 1;
+            else
+            {
+                result++;
+
+                stack.Push(node.right);
+                stack.Push(node.left);
+            }
+        }
+
+        return result;
+    }
+
+    // Accepted solution that runs for 1ms and has O(n) time complexity
+    // public int CountNodes(TreeNode root) 
+    // {
+    //     var stack = new Stack<TreeNode?>();
+    //     int result = 0;
+
+    //     stack.Push(root);
+
+    //     while(stack.Count > 0)
+    //     {
+    //         var node = stack.Pop();
+
+    //         if (node is not null)
+    //         {
+    //             result++;
+    //             stack.Push(node.right);
+    //             stack.Push(node.left);
+    //         }
+    //     }
+
+    //     return result;
+    // }
+}
--- LeetCode.CSharp/Program.cs
+++ LeetCode.CSharp/Program.cs
@@ -1,5 +1,11 @@
-﻿var obj = new HappyNumber.Solution();
+﻿var root = new CountCompleteTreeNodes.TreeNode(1,
+    new CountCompleteTreeNodes.TreeNode(2,
+        new CountCompleteTreeNodes.TreeNode(4),
+        new CountCompleteTreeNodes.TreeNode(5)),
+    new CountCompleteTreeNodes.TreeNode(3,
+        new CountCompleteTreeNodes.TreeNode(6)));
 
-var result = obj.IsHappy(1);
+var obj = new CountCompleteTreeNodes.Solution();
+var result = obj.CountNodes(root);
 
-Console.WriteLine(result);
+Console.WriteLine(result); // Expected: 6

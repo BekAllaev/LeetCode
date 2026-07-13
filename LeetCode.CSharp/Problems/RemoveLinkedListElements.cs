6d42cde7aa491c25923a1710bd7f24c3b733ad95
Bekzod Allaev <Allaev_@outlook.com>
2026-07-13 15:14:51 +0200

    Switch to RemoveLinkedListElements problem and add solution
    Updated Program.cs to test RemoveLinkedListElements.Solution with a sample linked list. Added RemoveLinkedListElements.cs, implementing ListNode and Solution classes with the RemoveElements method to remove nodes with a given value.

---
+++ LeetCode.CSharp/Problems/RemoveLinkedListElements.cs
@@ -0,0 +1,37 @@
+﻿namespace RemoveLinkedListElements;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        var dummy = new ListNode();
+        ListNode curr = dummy;
+
+        while (head is not null)
+        {
+            if (head.val != val)
+            {
+                curr.next = head;
+                curr = curr.next;
+            }
+            head = head.next;
+        }
+
+        // So basically this line handles the case when the item to remove is at the end.
+        // In if statement we move curr every time we head.val != val. In case when head is the last node
+        // we won't move the curr and the curr.next will point to the ListNode that should be removed. So this logic works
+        curr.next = null!;
+        return dummy.next;
+    }
+}

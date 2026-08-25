0a70981e8161deb9a90583095429b1f8d307d490
Bekzod Allaev <Allaev_@outlook.com>
2026-08-25 12:37:29 +0200

    Switch entry point to SubarraySumEqualsK, add solution
    Updated Program.cs to use SubarraySumEqualsK.Solution and revised test cases accordingly. Added SubarraySumEqualsK.cs implementing an efficient hash map-based solution for counting subarrays with sum equal to k.

---
+++ LeetCode.CSharp/Problems/SubarraySumEqualsK.cs
@@ -0,0 +1,27 @@
+﻿namespace SubarraySumEqualsK;
+
+public class Solution
+{
+    public int SubarraySum(int[] nums, int k)
+    {
+        Dictionary<int, int> map = new();
+        int counter = 0, sum = 0;
+
+        map.Add(0, 1);
+
+        foreach (var item in nums)
+        {
+            sum += item;
+
+            if (map.ContainsKey(sum - k))
+                counter += map[sum - k];
+
+            if (map.ContainsKey(sum))
+                map[sum]++;
+            else
+                map.Add(sum, 1);
+        }
+
+        return counter;
+    }
+}
--- LeetCode.CSharp/Program.cs
+++ LeetCode.CSharp/Program.cs
@@ -1,5 +1,4 @@
-﻿var obj = new LongestConsecutiveSubsequence.Solution();
+﻿var obj = new SubarraySumEqualsK.Solution();
 
-Console.WriteLine(obj.LongestConsecutive([1, 0, 1, 2]));
-Console.WriteLine(obj.LongestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
-Console.WriteLine(obj.LongestConsecutive([100, 4, 200, 1, 3, 2]));
+Console.WriteLine(obj.SubarraySum([1, 1, 1], 2));
+Console.WriteLine(obj.SubarraySum([1], 0));

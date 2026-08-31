3c5d54f00e8213fb0d2387e00333e1fb99e1ffd8
Bekzod Allaev <Allaev_@outlook.com>
2026-08-31 12:51:54 +0200

    Optimize prefix sum tracking with array-based approach
    Replaced Dictionary with an array of size 2n+1 for prefix sum
    index mapping to improve performance and reduce memory usage.
    Initialized array with int.MinValue and set center index to -1
    for sum 0. Retained original Dictionary code as comments for
    reference.

--- LeetCode.CSharp/Problems/ContigousArray.cs
+++ LeetCode.CSharp/Problems/ContigousArray.cs
@@ -2,27 +2,61 @@
 
 public class Solution
 {
+    // Time complexity - O(n)
+    // Space complexity - O(n)
+    // Runs for 8ms
     public int FindMaxLength(int[] nums)
     {
-        Dictionary<int, int> map = new() { [0] = -1 };
+        int n = nums.Length;
+        int[] arr = new int[2 * n + 1];
+        Array.Fill(arr, int.MinValue); // instead of int.MinValue can be -2 too. 
+        arr[n] = -1; // set initial value
+
         int max = 0, sum = 0;
 
-        for (int i = 0; i < nums.Length; i++)
+        for (int i = 0; i < n; i++)
         {
             nums[i] = nums[i] == 0 ? -1 : 1;
-
             sum += nums[i];
 
-            if (map.ContainsKey(sum))
-            {
-                var length = i - map[sum];
+            int idx = sum + n; // Value can range from -n to n, thus this formula works
 
+            if (arr[idx] != int.MinValue)
+            {
+                int length = i - arr[idx];
                 max = length > max ? length : max;
             }
             else
-                map.Add(sum, i);
+                arr[idx] = i;
         }
 
         return max;
     }
+
+    // Time complexity - O(n)
+    // Space complexity - O(n)
+    // Runs for 12ms
+    //public int FindMaxLength(int[] nums)
+    //{
+    //    Dictionary<int, int> map = new() { [0] = -1 };
+    //    int max = 0, sum = 0;
+
+    //    for (int i = 0; i < nums.Length; i++)
+    //    {
+    //        nums[i] = nums[i] == 0 ? -1 : 1;
+
+    //        sum += nums[i];
+
+    //        if (map.ContainsKey(sum))
+    //        {
+    //            var length = i - map[sum];
+
+    //            max = length > max ? length : max;
+    //        }
+    //        else
+    //            map.Add(sum, i);
+    //    }
+
+    //    return max;
+    //}
 }

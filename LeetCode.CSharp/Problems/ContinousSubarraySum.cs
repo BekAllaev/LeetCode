9d3b933602c4f674d59b8cc265e57378abbf4266
Bekzod Allaev <Allaev_@outlook.com>
2026-09-04 12:03:51 +0200

    Implement CheckSubarraySum to find subarrays with sum multiple of k
    Added ContinousSubarraySum namespace and Solution class with CheckSubarraySum method. Utilizes a dictionary to track prefix sums modulo k, efficiently detecting continuous subarrays of at least two elements whose sum is a multiple of k.

---
+++ LeetCode.CSharp/Problems/ContinousSubarraySum.cs
@@ -0,0 +1,25 @@
+﻿namespace ContinousSubarraySum;
+
+public class Solution
+{
+    // Solution that runs for 16ms
+    // TC - O(n)
+    // SC - O(n)
+    public bool CheckSubarraySum(int[] nums, int k)
+    {
+        Dictionary<int, int> map = new() { [0] = -1 };
+        int sum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+
+            if (map.ContainsKey(sum % k) && i - map[sum % k] >= 2)
+                return true;
+            else if (!map.ContainsKey(sum % k))
+                map.Add(sum % k, i);
+        }
+
+        return false;
+    }
+}

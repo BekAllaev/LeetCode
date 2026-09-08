134e446861f9366aaf6a001b1ee173084f11d726
Bekzod Allaev <Allaev_@outlook.com>
2026-09-08 16:42:59 +0200

    Add Solution class with TwoSum method using dictionary
    Added Solution class in TwoSum namespace. Implemented TwoSum method to find two indices whose values sum to the target using a dictionary. Included necessary using directives and returned indices as an array.

---
+++ LeetCode.CSharp/Problems/TwoSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoSum;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new();
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var key = target - nums[i];
+            if (map.ContainsKey(key))
+            {
+                result[0] = i;
+                result[1] = map[key];
+
+                break;
+            }
+            else if (!map.ContainsKey(nums[i]))
+                map.Add(nums[i], i);
+        }
+
+        return result;
+    }
+}

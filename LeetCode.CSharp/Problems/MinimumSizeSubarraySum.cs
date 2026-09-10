22255860b76a56787be3efe7372b6953cf122c4a
Bekzod Allaev <Allaev_@outlook.com>
2026-09-10 17:07:22 +0200

    Switch Program.cs to MinimumSizeSubarraySum, add solution
    Updated Program.cs to use MinimumSizeSubarraySum.Solution and test MinSubArrayLen with sample input. Added MinimumSizeSubarraySum.cs with an efficient implementation of MinSubArrayLen to find the minimal length of a contiguous subarray with sum at least the target.

---
+++ LeetCode.CSharp/Problems/MinimumSizeSubarraySum.cs
@@ -0,0 +1,32 @@
+﻿namespace MinimumSizeSubarraySum;
+
+public class Solution
+{
+    // Accepted solution that runs for 1ms and beats 59.42% of solutions
+    // TC - O(n)
+    // SC - O(n)
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+        int left = 0, right = 0, min = -1;
+        int[] prefixSum = new int[nums.Length + 1];
+        prefixSum[0] = 0;
+
+        for (int i = 1; i < prefixSum.Length; i++)
+            prefixSum[i] = nums[i - 1] + prefixSum[i - 1];
+
+        while (left < nums.Length)
+        {
+            // sum of the subarray between left + 1 and right
+            while (prefixSum[right] - prefixSum[left] < target && right < nums.Length)
+                right++;
+
+            var len = right - left;
+            if (prefixSum[right] - prefixSum[left] >= target && (min == -1 || len < min))
+                min = len;
+
+            left++;
+        }
+
+        return min == -1 ? 0 : min;
+    }
+}
--- LeetCode.CSharp/Program.cs
+++ LeetCode.CSharp/Program.cs
@@ -1,3 +1,3 @@
-﻿var obj = new ThreeSum.Solution();
+﻿var obj = new MinimumSizeSubarraySum.Solution();
 
-Console.WriteLine(obj.ThreeSum([1, 2, 0, 1, 0, 0, 0, 0]));
+Console.WriteLine(obj.MinSubArrayLen(7, [2, 3, 1, 2, 4, 3]));

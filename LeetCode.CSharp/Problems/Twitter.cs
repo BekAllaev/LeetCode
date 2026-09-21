b05aef7b83b4a77048f44ca2269a8f6b45ebf333
Bekzod Allaev <Allaev_@outlook.com>
2026-09-21 17:43:00 +0200

    Switch to Twitter class; add Twitter.cs implementation
    Program.cs now uses the new Twitter class from LeetCode.CSharp.Problems, replacing previous ListNode code. Added Twitter.cs, which implements posting tweets, following/unfollowing users, and retrieving news feeds using arrays and linked lists to manage user data and tweet order. Includes test calls simulating user actions.

---
+++ LeetCode.CSharp/Problems/Twitter.cs
@@ -0,0 +1,127 @@
+﻿namespace LeetCode.CSharp.Problems;
+
+public class Twitter
+{
+    int postCount = 0;
+    bool[][] network = new bool[500][];
+    LinkedList<(int, int)>[] usersFeed = new LinkedList<(int, int)>[500];
+    LinkedList<(int, int)>[] usersTweets = new LinkedList<(int, int)>[500];
+
+    public Twitter() { }
+
+    public void PostTweet(int userId, int tweetId)
+    {
+        postCount++;
+        UpdateFeed(userId - 1, tweetId, postCount);
+        AddTweet(userId - 1, tweetId, postCount);
+
+        var followers = network[userId - 1];
+
+        for (int i = 0; followers is not null && i < followers.Length; i++)
+        {
+            if (followers[i])
+                UpdateFeed(i, tweetId, postCount);
+        }
+    }
+
+    public IList<int> GetNewsFeed(int userId)
+        => usersFeed[userId - 1] is not null ? usersFeed[userId - 1].Take(10).Select(x => x.Item1).ToList() : new List<int>();
+
+    public void Follow(int followerId, int followeeId)
+    {
+        var followers = network[followeeId - 1];
+
+        if (followers is null)
+        {
+            followers = new bool[500];
+            network[followeeId - 1] = followers;
+        }
+
+        network[followeeId - 1][followerId - 1] = true;
+
+        var followeeTweets = usersTweets[followeeId - 1];
+
+        if (followeeTweets is null)
+            return;
+
+        foreach (var tweet in followeeTweets)
+        {
+            UpdateFeed(followerId - 1, tweet.Item1, tweet.Item2);
+        }
+    }
+
+    public void Unfollow(int followerId, int followeeId)
+    {
+        var followers = network[followeeId - 1];
+
+        if (followers is null)
+            return;
+
+        followers[followerId - 1] = false;
+        var followeeTweets = usersTweets[followeeId - 1];
+        var followerFeed = usersFeed[followerId - 1];
+
+        if (followerFeed is null)
+            return;
+
+        foreach (var feedTweet in followerFeed.ToList())
+        {
+            if (followeeTweets.Contains(feedTweet))
+                followerFeed.Remove(feedTweet);
+        }
+    }
+
+    private void UpdateFeed(int userId, int tweetId, int postCount)
+    {
+        var userFeed = usersFeed[userId];
+
+        if (userFeed is null)
+        {
+            userFeed = new LinkedList<(int, int)>();
+            usersFeed[userId] = userFeed;
+        }
+
+        if (!userFeed.Contains((tweetId, postCount)))
+        {
+            var cur = userFeed.Last;
+            while (cur is not null)
+            {
+                if (cur.Value.Item2 < postCount)
+                    cur = cur.Previous;
+                else break;
+            }
+
+            if (cur is not null)
+                userFeed.AddAfter(cur, (tweetId, postCount));
+            else
+                userFeed.AddFirst((tweetId, postCount));
+        }
+
+        /*
+        if (userFeed.Count > 10)
+            userFeed.RemoveLast();
+        */
+    }
+
+    private void AddTweet(int userId, int tweetId, int postCount)
+    {
+        var userTweets = usersTweets[userId];
+
+        if (userTweets is null)
+        {
+            userTweets = new LinkedList<(int, int)>();
+            usersTweets[userId] = userTweets;
+        }
+
+        userTweets.AddFirst((tweetId, postCount));
+    }
+}
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.PostTweet(userId,tweetId);
+ * IList<int> param_2 = obj.GetNewsFeed(userId);
+ * obj.Follow(followerId,followeeId);
+ * obj.Unfollow(followerId,followeeId);
+ */
--- LeetCode.CSharp/Program.cs
+++ LeetCode.CSharp/Program.cs
@@ -1,9 +1,36 @@
-﻿using Task19;
+﻿using LeetCode.CSharp.Problems;
 
-var obj = new Task19.Solution();
+var twitter = new Twitter();
 
-//ListNode node = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-//ListNode node = new ListNode(1, new ListNode(2));
-ListNode node = new ListNode(1);
+//twitter.PostTweet(1, 5);
+//twitter.GetNewsFeed(1);
+//twitter.Follow(1, 2);
+//twitter.PostTweet(2, 6);
+//twitter.GetNewsFeed(1);
+//twitter.Unfollow(1, 2);
+//twitter.GetNewsFeed(1);
 
-obj.RemoveNthFromEnd(node, 1);
+//twitter.PostTweet(1, 1);
+//twitter.GetNewsFeed(1);
+//twitter.Follow(2, 1);
+//twitter.GetNewsFeed(2);
+//twitter.Unfollow(2, 1);
+//twitter.GetNewsFeed(2);
+
+/*
+ [[],[1,5],[1,3],[1,101],[1,13],[1,10],[1,2],[1,94],[1,505],[1,333],[1,22],[1,11],[1]]
+ */
+
+twitter.PostTweet(2, 5);
+twitter.PostTweet(1, 3);
+twitter.PostTweet(1, 101);
+twitter.PostTweet(2, 13);
+twitter.PostTweet(2, 10);
+twitter.PostTweet(1, 2);
+twitter.PostTweet(2, 94);
+twitter.PostTweet(2, 505);
+twitter.PostTweet(1, 333);
+twitter.PostTweet(1, 22);
+twitter.Follow(2, 1);
+twitter.Unfollow(2, 1);
+twitter.GetNewsFeed(1);

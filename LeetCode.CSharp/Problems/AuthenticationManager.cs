5c0a63d2c4d3241d3b674ec0aad8061215cdb915
Bekzod Allaev <Allaev_@outlook.com>
2026-09-23 13:52:18 +0200

    Add AuthenticationManager and update tests in Program.cs
    Replaced Twitter test code in Program.cs with tests for the new AuthenticationManager class, which manages token generation, renewal, and expiration. Added AuthenticationManager.cs implementing efficient token management using a linked list and dictionary. Included an alternative, commented-out implementation for reference.

---
+++ LeetCode.CSharp/Problems/AuthenticationManager.cs
@@ -0,0 +1,150 @@
+﻿namespace LeetCode.CSharp.Problems;
+
+// Accepted solution for 5ms
+public class AuthenticationManager
+{
+    private readonly int _timeToLive;
+    private readonly Dictionary<string, LinkedListNode<Token>> _tokensNode;
+    private readonly LinkedList<Token> _tokens;
+
+    public AuthenticationManager(int timeToLive)
+    {
+        _timeToLive = timeToLive;
+        _tokensNode = new();
+        _tokens = new();
+    }
+
+    public void Generate(string tokenId, int currentTime)
+    {
+        DeleteExpiredTokens(currentTime);
+
+        if (_tokensNode.ContainsKey(tokenId))
+            return;
+
+        var token = new Token(tokenId, currentTime, _timeToLive);
+        var tokenNode = _tokens.AddLast(token);
+        _tokensNode[tokenId] = tokenNode;
+    }
+
+    public void Renew(string tokenId, int currentTime)
+    {
+        DeleteExpiredTokens(currentTime);
+
+        if (!_tokensNode.ContainsKey(tokenId))
+            return;
+
+        var tokenNode = _tokensNode[tokenId];
+        tokenNode.Value.Renew(currentTime);
+        _tokens.Remove(tokenNode);
+        _tokens.AddLast(tokenNode);
+    }
+
+    public int CountUnexpiredTokens(int currentTime)
+    {
+        DeleteExpiredTokens(currentTime);
+
+        return _tokens.Count;
+    }
+
+    private void DeleteExpiredTokens(int currentTime)
+    {
+        var cur = _tokens.First;
+
+        while (cur is not null)
+        {
+            var next = cur.Next;
+            if (cur.Value.IsExpired(currentTime))
+            {
+                _tokensNode.Remove(cur.Value.Id);
+                _tokens.Remove(cur);
+                cur = next;
+            }
+            else
+                break;
+        }
+    }
+
+    class Token
+    {
+        private string _id;
+        private int _expireTime, _timeToLive;
+
+        public Token(string id, int currentTime, int timeToLive)
+        {
+            _id = id;
+            _timeToLive = timeToLive;
+            _expireTime = currentTime + timeToLive;
+        }
+
+        public string Id => _id;
+
+        public void Renew(int currentTime)
+        {
+            if (!IsExpired(currentTime))
+                _expireTime = currentTime + _timeToLive;
+        }
+
+        public bool IsExpired(int currentTime) => _expireTime <= currentTime;
+    }
+}
+
+// Accepted solution that runs for 68ms
+//public class AuthenticationManager
+//{
+//    private readonly int _timeToLive;
+//    private readonly Dictionary<string, Token> _tokens;
+//    private readonly Dictionary<int, int> _activeTokens;
+
+//    public AuthenticationManager(int timeToLive)
+//    {
+//        _timeToLive = timeToLive;
+//        _tokens = new();
+//        _activeTokens = new();
+//    }
+
+//    public void Generate(string tokenId, int currentTime)
+//    {
+//        if (_tokens.ContainsKey(tokenId))
+//            return;
+
+//        _tokens[tokenId] = new Token(tokenId, currentTime, _timeToLive);
+
+//        var key = currentTime + _timeToLive - 1;
+//        _activeTokens[key] =
+//            _activeTokens.ContainsKey(key) ?
+//                ++_activeTokens[key] :
+//                1;
+//    }
+
+//    public void Renew(string tokenId, int currentTime)
+//    {
+//        if (!_tokens.ContainsKey(tokenId))
+//            return;
+
+//        var token = _tokens[tokenId];
+//        token.Renew(currentTime);
+//    }
+
+//    public int CountUnexpiredTokens(int currentTime) => _activeTokens[currentTime];
+
+//    class Token
+//    {
+//        private string _id;
+//        private int _expireTime, _timeToLive;
+
+//        public Token(string id, int currentTime, int timeToLive)
+//        {
+//            _id = id;
+//            _timeToLive = timeToLive;
+//            _expireTime = currentTime + timeToLive;
+//        }
+
+//        public void Renew(int currentTime)
+//        {
+//            if (!IsExpired(currentTime))
+//                _expireTime = currentTime + _timeToLive;
+//        }
+
+//        public bool IsExpired(int currentTime) => _expireTime <= currentTime;
+//    }
+//}
--- LeetCode.CSharp/Program.cs
+++ LeetCode.CSharp/Program.cs
@@ -1,44 +1,22 @@
 ﻿using LeetCode.CSharp.Problems;
 
-var twitter = new Twitter();
+var authManager = new AuthenticationManager(13);
 
-twitter.PostTweet(1, 5);
-twitter.GetNewsFeed(1);
-twitter.Follow(1, 2);
-twitter.PostTweet(2, 6);
-twitter.GetNewsFeed(1);
-twitter.Unfollow(1, 2);
-twitter.GetNewsFeed(1);
-
-//twitter.PostTweet(1, 1);
-//twitter.GetNewsFeed(1);
-//twitter.Follow(2, 1);
-//twitter.GetNewsFeed(2);
-//twitter.Unfollow(2, 1);
-//twitter.GetNewsFeed(2);
-
-/*
- [[],[1,5],[1,3],[1,101],[1,13],[1,10],[1,2],[1,94],[1,505],[1,333],[1,22],[1,11],[1]]
- */
-
-//twitter.PostTweet(2, 5);
-//twitter.PostTweet(1, 3);
-//twitter.PostTweet(1, 101);
-//twitter.PostTweet(2, 13);
-//twitter.PostTweet(2, 10);
-//twitter.PostTweet(1, 2);
-//twitter.PostTweet(2, 94);
-//twitter.PostTweet(2, 505);
-//twitter.PostTweet(1, 333);
-//twitter.PostTweet(1, 22);
-//twitter.Follow(2, 1);
-//twitter.Unfollow(2, 1);
-//twitter.GetNewsFeed(1);
-
-//twitter.PostTweet(2, 5);
-//twitter.Follow(1, 2);
-//twitter.Follow(1, 2);
-
-//twitter.PostTweet(1, 1);
-//twitter.Follow(2, 1);
-//twitter.Unfollow(2, 1);
+authManager.Renew("ajvy", 1);
+authManager.CountUnexpiredTokens(3);
+authManager.CountUnexpiredTokens(4);
+authManager.Generate("fuzzq", 5);
+authManager.Generate("izmry", 7);
+authManager.Renew("puv", 12);
+authManager.Generate("ybiqb", 13);
+authManager.Generate("gm", 14);
+authManager.CountUnexpiredTokens(15);
+authManager.CountUnexpiredTokens(18);
+authManager.CountUnexpiredTokens(19);
+authManager.Renew("ybiqb", 21);
+authManager.CountUnexpiredTokens(23);
+authManager.CountUnexpiredTokens(25);
+authManager.CountUnexpiredTokens(26);
+authManager.Generate("aqdm", 28);
+Console.WriteLine(authManager.CountUnexpiredTokens(29));
+authManager.Renew("puv", 30);
